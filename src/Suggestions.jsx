cb5592b23165acdf32192513a0064a54cdb36f58
SrinathNagaraj22 <srinathn.2212@gmail.com>
2025-09-12 16:14:22 +0530

    feat:Profileandsuggestion

--- src/Suggestions.jsx
+++ src/Suggestions.jsx
@@ -1,29 +1,56 @@
 import React, {useEffect, useState} from 'react'
 
 function Suggestions() {
-  const [profiles, setProfile]= useState(null);
+  const [profile, setProfile]= useState(null);
   const [Suggestions, setSuggestions] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:3000/profiles')
+    fetch('http://localhost:3000/profile')
     .then(data=>data.json())
-    .then(data=>setProfile(data))
+    .then(data=>setProfile(data[0]))
     .catch(err=>console.log(err))
 
-    // fetch('http://localhost:3000/suggestions')
-    // .then(data=>data.json())
-    // .then(data=>setsuggestions(data))
-    // .catch(err=>console.log(err))
+    fetch('http://localhost:3000/suggestions')
+    .then(data=>data.json())
+    .then(data=>setSuggestions(data))
+    .catch(err=>console.log(err))
   },[]);
 
   return (
     <div>
+      <div className='suggestions w-75 m-4'>
+      {profile && (
       <div className='d-flex'>
-        <img className='dp rounded-circle' src={profiles.profilePic} alt='profilepicture' />
-        <h5>{profiles.username}</h5>
+        <img className='dp rounded-circle mt-3 ms-4' src={profile.profilePic} alt="profilepicture"  />
+        <h5 className='my-3'>{profile.username}</h5>
+        <small className='ms-auto text-primary mt-3 '>Switch</small>
       </div>
-    </div>
+      )}
+      <div className='d-flex'>
+        <h5 className='mx-4 mt-2 mb-0'>Suggested for you</h5>
+        <small className='ms-auto mt-2'>See All</small>
+      </div>
+      </div>
+      {Suggestions.length>0 ?(
+      <div className='w-75 ms-4 mt-2'>
+        {Suggestions.map((suggestion)=>(
+          <div className='ms-4 my-1' key={suggestion.id}>
+            <div className='d-flex'>
+              <img className='dp rounded-circle' src={suggestion.profilePic} alt='suggestionprofilepic' />
+              <h5 className='suggestionsusername  my-2'>{suggestion.username}</h5>
+              <small className='ms-auto text-primary my-2 '>Follow</small>
+            </div>
+          </div>
+        ))}
+      </div>
+    ):(
+      <div>
+        Loading
+      </div>
+      )}
+  </div>
   )
 }
+
 
 export default Suggestions
--- src/stories.jsx
+++ src/stories.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Stories() {
+  const[Stories, setStories]=useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3000/story')
+    .then(data=>data.json())
+    .then(data=>setStories(data))
+  })
   return (
-    <div className='story bg-info secondary'>
-      stories
+    <div className='story d-flex ms-4 mt-3'>
+      {Stories.length>0?(
+        Stories.map((story)=>(
+          <div className='ms-2' key={story.id}>
+            <div className='gradient-border'>
+              <img src={story.user.profilePic} alt="storiesprofilepicture" className='story-dp rounded-circle' />
+            </div>
+            <p className='text-truncate' style={{width:"50px"}}>{story.user.username}</p>
+          </div>
+        ))
+      ):(
+        <div>
+          <p>Loading</p>
+        </div>
+      )} 
     </div>
   )
 }
